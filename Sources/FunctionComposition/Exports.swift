303a7ac8288f0c72bed486c58f53a1930600c1ce
maximkrouk <id.maximkrouk@gmail.com>
2026-04-13 09:56:03 +0200

    feat: Add missing exports
    # Conflicts:
    #	Sources/FunctionComposition/Exports.swift

--- Sources/FunctionComposition/Exports.swift
+++ Sources/FunctionComposition/Exports.swift
@@ -1,7 +1,13 @@
 #if NominalTypes && canImport(NominalFunctionTypesComposition)
 @_exported import NominalFunctionTypesComposition
+#endif
 
 // _Currying is already exported from NominalFunctionTypesComposition if available
-#elseif !NominalTypes && Currying && canImport(_Currying)
+#if !NominalTypes && Currying && canImport(_Currying)
 @_exported import _Currying
+#endif
+
+// _Tuples is already exported from NominalFunctionTypesComposition if available
+#if !NominalTypes && Tuples && canImport(_Tuples)
+@_exported import _Tuples
 #endif
